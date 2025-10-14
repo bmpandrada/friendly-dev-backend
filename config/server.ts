27ce006e571f94d5906a516fc3722fff4c35bc88
bruce <bmpandrada@gmail.com>
2025-10-14 11:50:45 +0800

    Fix Strapi public URL for Render

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,7 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://friendly-dev-backend-pmaj.onrender.com'),
   app: {
     keys: env.array('APP_KEYS'),
   },
